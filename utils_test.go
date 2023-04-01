a8acf8a271b5fef50736cac231f1b6a7c4d28ae4
liran <857271370@qq.com>
2023-04-02 06:44:21 +0800

    .

--- utils_test.go
+++ utils_test.go
@@ -26,9 +26,9 @@ func TestGetModelName(t *testing.T) {
 
 func TestGetValueOfModelPrimaryKey(t *testing.T) {
 	type User struct {
-		Name       string `bson:"_id"`
-		Age        int64  `db:"pk"`
-		OrderCount int64
+		Name       string `json:"name" bson:"_id,omitempty"`
+		Age        int64  `json:"age" bson:"age,omitempty"`
+		OrderCount int64  `json:"order_count" bson:"order_count,omitempty"`
 	}
 	pk := GetValueOfModelPrimaryKey(&User{Name: "liran", Age: 132})
 	if pk != "liran" {
