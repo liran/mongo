00ab4cd72a4176c4f22546522e6e66eea2cf0f54
liran <857271370@qq.com>
2023-04-03 21:46:29 +0800

    .

--- error.go
+++ error.go
@@ -4,6 +4,6 @@ import "github.com/pkg/errors"
 
 var (
 	ErrInvalidModelName = errors.New("invalid model name")
-	ErrNoID             = errors.New(`no id. not found primary key from model, defined by tag db:"pk"`)
+	ErrNoID             = errors.New(`no id. not found primary key from model, defined by tag db:"pk" or bson:"_id"`)
 	ErrNotFoundModel    = errors.New("not found model")
 )
